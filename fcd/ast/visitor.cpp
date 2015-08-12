e37b07be4f4c97a88e0d36c04672839810591376
Félix Cloutier <felix@felixcloutier.com>
2015-08-12 13:18:00 -0400

    Implemented base visitation methods

--- fcd/ast/visitor.cpp
+++ fcd/ast/visitor.cpp
@@ -20,3 +20,85 @@
 //
 
 #include "visitor.h"
+
+void StatementVisitor::visitSequence(SequenceNode* sequence)
+{
+	for (auto statement : sequence->statements)
+	{
+		statement->visit(*this);
+	}
+}
+
+void StatementVisitor::visitIfElse(IfElseNode* ifElse)
+{
+	ifElse->ifBody->visit(*this);
+	if (ifElse->elseBody != nullptr)
+	{
+		ifElse->elseBody->visit(*this);
+	}
+}
+
+void StatementVisitor::visitLoop(LoopNode* loop)
+{
+	loop->loopBody->visit(*this);
+}
+
+void StatementVisitor::visitKeyword(KeywordNode* keyword)
+{
+}
+
+void StatementVisitor::visitExpression(ExpressionNode* expression)
+{
+}
+
+void StatementVisitor::visitDeclaration(DeclarationNode* declaration)
+{
+}
+
+void StatementVisitor::visitAssignment(AssignmentNode* assignment)
+{
+}
+
+
+void ExpressionVisitor::visitUnary(UnaryOperatorExpression* unary)
+{
+	unary->operand->visit(*this);
+}
+
+void ExpressionVisitor::visitNAry(NAryOperatorExpression* nary)
+{
+	for (auto expr : nary->operands)
+	{
+		expr->visit(*this);
+	}
+}
+
+void ExpressionVisitor::visitTernary(TernaryExpression* ternary)
+{
+	ternary->condition->visit(*this);
+	ternary->ifTrue->visit(*this);
+	ternary->ifFalse->visit(*this);
+}
+
+void ExpressionVisitor::visitNumeric(NumericExpression* numeric)
+{
+}
+
+void ExpressionVisitor::visitToken(TokenExpression* token)
+{
+}
+
+void ExpressionVisitor::visitCall(CallExpression* call)
+{
+	call->callee->visit(*this);
+	for (auto arg : call->parameters)
+	{
+		arg->visit(*this);
+	}
+}
+
+void ExpressionVisitor::visitCast(CastExpression* cast)
+{
+	cast->type->visit(*this);
+	cast->casted->visit(*this);
+}
