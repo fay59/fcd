37595b15e4899feb940a2a1912c8217263871174
Félix Cloutier <felix@felixcloutier.com>
2015-10-23 17:38:33 -0400

    Created condition simplification pass skeleton

---
+++ fcd/pass_conditions.cpp
@@ -0,0 +1,52 @@
+//
+// pass_conditions.cpp
+// Copyright (C) 2015 Félix Cloutier.
+// All Rights Reserved.
+//
+// This file is part of fcd.
+// 
+// fcd is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// fcd is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with fcd.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "passes.h"
+
+SILENCE_LLVM_WARNINGS_BEGIN()
+SILENCE_LLVM_WARNINGS_END()
+
+using namespace llvm;
+using namespace std;
+
+namespace
+{
+	struct ConditionSimplification : public FunctionPass
+	{
+		static char ID;
+		
+		ConditionSimplification() : FunctionPass(ID)
+		{
+		}
+		
+		virtual bool runOnFunction(Function& fn) override
+		{
+			return false;
+		}
+	};
+	
+	char ConditionSimplification::ID = 0;
+}
+
+FunctionPass* createConditionSimplificationPass()
+{
+	return new ConditionSimplification;
+}
