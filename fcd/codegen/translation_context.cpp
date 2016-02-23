0d62d962e04798c86f8d957f13753603985c1e19
Félix Cloutier <felix@felixcloutier.com>
2016-02-23 16:03:03 -0500

    Refactor CloningDIrector away

--- fcd/codegen/translation_context.cpp
+++ fcd/codegen/translation_context.cpp
@@ -56,206 +56,6 @@ namespace
 		}
 	}
 	
-	class TranslationCloningDirector final : public CloningDirector
-	{
-		Module& module;
-		AddressToFunction& functionMap;
-		AddressToBlock& blockMap;
-		vector<const CallInst*> delayedJumps;
-		vector<const CallInst*> delayedCalls;
-		
-		static Type* getMemoryType(LLVMContext& ctx, size_t size)
-		{
-			if (size == 1 || size == 2 || size == 4 || size == 8)
-			{
-				return Type::getIntNTy(ctx, static_cast<unsigned>(size * 8));
-			}
-			llvm_unreachable("invalid pointer size");
-		}
-		
-		CloningAction fixFcdIntrinsic(ValueToValueMapTy& vmap, const CallInst& call, BasicBlock* bb)
-		{
-			Function* called = call.getCalledFunction();
-			if (called == nullptr)
-			{
-				return CloneInstruction;
-			}
-			
-			auto name = called->getName();
-			if (name == "x86_jump_intrin")
-			{
-				// At this point there is probably not enough information to infer the
-				// destination of the jump (or call) because CloneAndPruneInto only
-				// simplifies PHINodes as it updates the CFG.
-				delayedJumps.push_back(&call);
-			}
-			else if (name == "x86_call_intrin")
-			{
-				delayedCalls.push_back(&call);
-			}
-			else if (name == "x86_ret_intrin")
-			{
-				auto ret = ReturnInst::Create(call.getContext(), bb);
-				md::setNonInlineReturn(*ret);
-				return StopCloningBB;
-			}
-			else if (name == "x86_read_mem")
-			{
-				Value* intptr = vmap[call.getOperand(0)];
-				ConstantInt* sizeOperand = cast<ConstantInt>(vmap[call.getOperand(1)]);
-				Type* loadType = getMemoryType(call.getContext(), sizeOperand->getLimitedValue());
-				CastInst* pointer = CastInst::Create(CastInst::IntToPtr, intptr, loadType->getPointerTo(), "", bb);
-				Instruction* replacement = new LoadInst(pointer, "", bb);
-				md::setProgramMemory(*replacement);
-				
-				Type* i64 = Type::getInt64Ty(call.getContext());
-				if (replacement->getType() != i64)
-				{
-					replacement = CastInst::Create(Instruction::ZExt, replacement, i64, "", bb);
-				}
-				vmap[&call] = replacement;
-				return SkipInstruction;
-			}
-			else if (name == "x86_write_mem")
-			{
-				Value* intptr = vmap[call.getOperand(0)];
-				Value* value = vmap[call.getOperand(2)];
-				ConstantInt* sizeOperand = cast<ConstantInt>(vmap[call.getOperand(1)]);
-				Type* storeType = getMemoryType(call.getContext(), sizeOperand->getLimitedValue());
-				CastInst* pointer = CastInst::Create(CastInst::IntToPtr, intptr, storeType->getPointerTo(), "", bb);
-				
-				if (value->getType() != storeType)
-				{
-					// Assumption: storeType can only be smaller than the type of storeValue
-					value = CastInst::Create(Instruction::Trunc, value, storeType, "", bb);
-				}
-				StoreInst* storeInst = new StoreInst(value, pointer, bb);
-				md::setProgramMemory(*storeInst);
-				return SkipInstruction;
-			}
-			
-			return CloneInstruction;
-		}
-		
-	public:
-		TranslationCloningDirector(Module& module, AddressToFunction& functionMap, AddressToBlock& blockMap)
-		: module(module), functionMap(functionMap), blockMap(blockMap)
-		{
-		}
-		
-		void performDelayedOperations(ValueToValueMapTy& vmap, SmallVectorImpl<ReturnInst*>& returns, uint64_t nextAddress)
-		{
-			for (ReturnInst* ret : returns)
-			{
-				if (!md::isNonInlineReturn(*ret))
-				{
-					BranchInst::Create(blockMap.blockToInstruction(nextAddress), ret);
-					ret->eraseFromParent();
-				}
-			}
-			
-			for (const CallInst* jump : delayedJumps)
-			{
-				if (auto translated = dyn_cast_or_null<CallInst>(vmap[jump]))
-				{
-					if (auto constantDestination = dyn_cast<ConstantInt>(translated->getOperand(2)))
-					{
-						BasicBlock* parent = translated->getParent();
-						BasicBlock* remainder = parent->splitBasicBlock(translated);
-						auto terminator = parent->getTerminator();
-						
-						uint64_t dest = constantDestination->getLimitedValue();
-						BasicBlock* destination = blockMap.blockToInstruction(dest);
-						BranchInst::Create(destination, terminator);
-						terminator->eraseFromParent();
-						remainder->eraseFromParent();
-					}
-					else
-					{
-						Function* intrin = jump->getCalledFunction();
-						Value* inThisModule = module.getOrInsertFunction(intrin->getName(), intrin->getFunctionType(), intrin->getAttributes());
-						translated->setCalledFunction(inThisModule);
-					}
-				}
-			}
-			
-			for (const CallInst* call : delayedCalls)
-			{
-				if (auto translated = dyn_cast_or_null<CallInst>(vmap[call]))
-				{
-					if (auto constantDestination = dyn_cast<ConstantInt>(translated->getOperand(2)))
-					{
-						uint64_t destination = constantDestination->getLimitedValue();
-						Function* target = functionMap.getCallTarget(destination);
-						CallInst* replacement = CallInst::Create(target, {translated->getOperand(1)}, "", translated);
-						translated->replaceAllUsesWith(replacement);
-						translated->eraseFromParent();
-					}
-					else
-					{
-						Function* intrin = call->getCalledFunction();
-						Value* inThisModule = module.getOrInsertFunction(intrin->getName(), intrin->getFunctionType(), intrin->getAttributes());
-						translated->setCalledFunction(inThisModule);
-					}
-				}
-			}
-			
-			delayedJumps.clear();
-			delayedCalls.clear();
-		}
-		
-		virtual CloningAction handleInstruction(ValueToValueMapTy& vmap, const Instruction* inst, BasicBlock* bb) override
-		{
-			if (auto call = dyn_cast<CallInst>(inst))
-			{
-				if (auto llvmIntrin = dyn_cast<IntrinsicInst>(call))
-				{
-					// the instruction cloner is a little dumb here so we need to tell it that
-					// intrinsics are different in different modules
-					Function* intrin = llvmIntrin->getCalledFunction();
-					auto& handle = vmap[intrin];
-					if (handle == nullptr)
-					{
-						handle = module.getOrInsertFunction(intrin->getName(), intrin->getFunctionType(), intrin->getAttributes());
-					}
-					return CloneInstruction;
-				}
-				else
-				{
-					return fixFcdIntrinsic(vmap, *call, bb);
-				}
-			}
-			else
-			{
-				return CloneInstruction;
-			}
-		}
-	};
-	
-	void inlineFunction(Function *target, Function *toInline, ArrayRef<llvm::Value *> parameters, TranslationCloningDirector &director, uint64_t nextAddress)
-	{
-		assert(toInline->arg_size() == parameters.size());
-		auto iter = toInline->arg_begin();
-		
-		ValueToValueMapTy valueMap;
-		for (Value* parameter : parameters)
-		{
-			valueMap[static_cast<Argument*>(iter)] = parameter;
-			++iter;
-		}
-		
-		SmallVector<ReturnInst*, 1> returns;
-		Function::iterator blockBeforeInstruction = target->back().getIterator();
-		CloneAndPruneIntoFromInst(target, toInline, toInline->front().begin(), valueMap, true, returns, "", nullptr, &director);
-		
-		// Stitch blocks together
-		Function::iterator firstNewBlock = blockBeforeInstruction;
-		++firstNewBlock;
-		BranchInst::Create(static_cast<BasicBlock*>(firstNewBlock), static_cast<BasicBlock*>(blockBeforeInstruction));
-		
-		director.performDelayedOperations(valueMap, returns, nextAddress);
-	}
-	
 	CallInformation infoForInstruction(TargetInfo& target, const cs_insn& inst)
 	{
 		const cs_detail& detail = *inst.detail;
@@ -471,7 +271,6 @@ Function* TranslationContext::createFunction(Executable& executable, uint64_t ba
 	auto targetInfo = TargetInfo::getTargetInfo(*module);
 	AddressToBlock blockMap(*fn);
 	BasicBlock* entry = &fn->back();
-	TranslationCloningDirector director(*module, *functionMap, blockMap);
 	
 	Argument* registers = static_cast<Argument*>(fn->arg_begin());
 	auto flags = new AllocaInst(irgen->getFlagsTy(), "flags", entry);
@@ -481,7 +280,7 @@ Function* TranslationContext::createFunction(Executable& executable, uint64_t ba
 	Type* ipType = GetElementPtrInst::getIndexedType(irgen->getRegisterTy(), ipGepIndices);
 	
 	Function* prologue = irgen->implementationForPrologue();
-	inlineFunction(fn, prologue, { configVariable, registers }, director, baseAddress);
+	irgen->inlineFunction(fn, prologue, { configVariable, registers }, *functionMap, blockMap, baseAddress);
 	
 	uint64_t addressToDisassemble;
 	auto end = executable.end();
@@ -504,7 +303,7 @@ Function* TranslationContext::createFunction(Executable& executable, uint64_t ba
 				// We have an implementation: inline it
 				Constant* detailAsConstant = irgen->constantForDetail(*inst->detail);
 				inliningParameters[1] = new GlobalVariable(*module, detailAsConstant->getType(), true, GlobalValue::PrivateLinkage, detailAsConstant);
-				inlineFunction(fn, implementation, inliningParameters, director, nextInstAddress);
+				irgen->inlineFunction(fn, implementation, inliningParameters, *functionMap, blockMap, nextInstAddress);
 			}
 			else
 			{
